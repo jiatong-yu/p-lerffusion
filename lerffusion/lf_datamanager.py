d7f8dfd8b9f68dad6b2e54ece788dfa285a222ff
DavidBorts <Davidaborts@gmail.com>
2023-05-07 23:43:49 -0400

    added masking starter code

--- lerffusion/lf_datamanager.py
+++ lerffusion/lf_datamanager.py
@@ -3,6 +3,7 @@
 Lerffusion Datamanager.
 TODO: 
 - ray sampling @david 
+- crop imgs
 """
 
 from __future__ import annotations
@@ -19,6 +20,14 @@ from nerfstudio.data.datamanagers.base_datamanager import (
     VanillaDataManager,
     VanillaDataManagerConfig,
 )
+
+# Our own imports
+import cv2
+#import PIL
+from transformers import YolosFeatureExtractor, YolosForObjectDetection
+#import torch
+import yolov5
+import numpy as np
 
 CONSOLE = Console(width=120)
 
@@ -66,7 +75,37 @@ class LerffusionDataManager(VanillaDataManager):
         self.original_image_batch['image'] = self.image_batch['image'].clone()
         self.original_image_batch['image_idx'] = self.image_batch['image_idx'].clone()
 
+        self.yolo = self.load_yolo5_model()
+        self.yolo_feature_extractor = YolosFeatureExtractor.from_pretrained('hustvl/yolos-tiny')
+        self.yolo_obj = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
+
+    def load_yolo5_model():
+        """
+        Returns:
+            Yolo5 model. see https://github.com/fcakyon/yolov5-pip for documentation
+        """
+        model = yolov5.load('yolov5s.pt')
+        model.conf = 0.25  # NMS confidence threshold
+        model.iou = 0.45  # NMS IoU threshold
+        model.agnostic = False  # NMS class-agnostic
+        model.multi_label = False  # NMS multiple labels per box
+        model.max_det = 20  # maximum number of detections per image
+        return model
+
     def get_mask(self, img):
+
+        #TODO @jiatong do you still need these?
+        #inputs = self.yolo_feature_extractor(images=img, return_tensors="pt")
+        #outputs = self.yolo_obj(**inputs)
+
+        predictions = self.yolo(img).pred[0]
+
+        box = predictions[int((predictions[:,5] == 47).nonzero(as_tuple=True)[0])][:4] #x1,y1,x2,y2
+        mask = np.zeros(img.shape)
+        #TODO @jiatong do we really need to be using cv2 here?
+        mask = cv2.rectangle(mask,pt1=(int(box[0]),int(box[1])),pt2=(int(box[2]),int(box[3])),color=(0,)*3,thickness=-1)
+
+        #TODO @jiatong finish me
         raise NotImplementedError
     
     def get_batch_masks(self, image_batch):
